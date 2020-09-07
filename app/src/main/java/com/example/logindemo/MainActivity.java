638dada767a4d6a860bf5160183748ab5891446d
ELCOT <v.deepikala@gmail.com>
2020-09-07 12:33:05 +0530

    Initial commit

---
+++ app/src/main/java/com/example/logindemo/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.logindemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText eName;
+    private EditText ePassword;
+    private Button eLogin;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        eName = findViewById(R.id.etName);
+        ePassword = findViewById(R.id.etPassword);
+        eLogin = findViewById(R.id.btnlogin);
+    }
+}
+
